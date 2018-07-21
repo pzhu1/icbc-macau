d154aff03cc37189b2c5f754999f9a14bc5783fd
Vincent <fly9879@263.net>
2018-07-21 17:31:12 +0800

    增加四個頁面

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,15 @@
-import { NgModule, ErrorHandler } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
-import { MyApp } from './app.component';
+import {NgModule, ErrorHandler} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {IonicApp, IonicModule, IonicErrorHandler} from 'ionic-angular';
+import {MyApp} from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
+import {AboutPage} from '../pages/about/about';
+import {ContactPage} from '../pages/contact/contact';
+import {HomePage} from '../pages/home/home';
+import {TabsPage} from '../pages/tabs/tabs';
 import {SigninPage} from "../pages/auth/signin";
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import {StatusBar} from '@ionic-native/status-bar';
+import {SplashScreen} from '@ionic-native/splash-screen';
 
 import {EffectsModule} from '@ngrx/effects';
 import {StoreModule} from '@ngrx/store';
@@ -17,52 +17,65 @@ import {StoreDevtoolsModule} from '@ngrx/store-devtools';
 import {NxModule} from '@nrwl/nx';
 import {reducers} from './app.reducer';
 import {counterReducer} from '../reducers/counter.reducer';
-import {counterInitialState} from'../status/counter.state';
+import {counterInitialState} from '../status/counter.state';
 import {CounterEffects} from '../effects/counter.effects';
 import {HttpClientModule} from '@angular/common/http';
 import {CounterService} from "../service/counter.service";
-import { NgxDatatableModule } from '@swimlane/ngx-datatable';
-import { JPush } from '@jiguang-ionic/jpush';
+import {NgxDatatableModule} from '@swimlane/ngx-datatable';
+import {JPush} from '@jiguang-ionic/jpush';
+import {Binsarch} from "../pages/home/services/binsarch";
+import {Custservice} from "../pages/home/services/custservice";
+import {Refund} from "../pages/home/services/refund";
+import {Wrongtrx} from "../pages/home/services/wrongtrx";
 
 //import {CounterService,Sse} from "../service/counter.service";
 @NgModule({
-  declarations: [
-    MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage,
-      SigninPage
-  ],
-  imports: [
-      HttpClientModule,
-    BrowserModule,
-      IonicModule.forRoot(MyApp),
+    declarations: [
+        MyApp,
+        AboutPage,
+        ContactPage,
+        HomePage,
+        TabsPage,
+        SigninPage,
+        Binsarch,
+        Custservice,
+        Refund,
+        Wrongtrx
+    ],
+    imports: [
+        HttpClientModule,
+        BrowserModule,
+        IonicModule.forRoot(MyApp),
 
-      StoreModule.forRoot(reducers),
-      EffectsModule.forRoot([]),
-      StoreDevtoolsModule.instrument({maxAge: 25}) ,
-      NxModule.forRoot(),
-      StoreModule.forFeature('counter', counterReducer, {initialState: counterInitialState}),
-      EffectsModule.forFeature([CounterEffects]),
-      NgxDatatableModule
+        StoreModule.forRoot(reducers),
+        EffectsModule.forRoot([]),
+        StoreDevtoolsModule.instrument({maxAge: 25}),
+        NxModule.forRoot(),
+        StoreModule.forFeature('counter', counterReducer, {initialState: counterInitialState}),
+        EffectsModule.forFeature([CounterEffects]),
+        NgxDatatableModule
 
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage,
-      SigninPage
-  ],
-  providers: [
-    StatusBar,
-    SplashScreen,
-      JPush,
-      CounterService,//Sse,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
-  ]
+    ],
+    bootstrap: [IonicApp],
+    entryComponents: [
+        MyApp,
+        AboutPage,
+        ContactPage,
+        HomePage,
+        TabsPage,
+        SigninPage,
+        Binsarch,
+        Custservice,
+        Refund,
+        Wrongtrx
+    ],
+    providers: [
+        StatusBar,
+        SplashScreen,
+        JPush,
+        CounterService,//Sse,
+        {provide: ErrorHandler, useClass: IonicErrorHandler}
+    ]
 })
-export class AppModule {}
+export class AppModule {
+}
